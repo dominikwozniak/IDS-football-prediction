5fe13308273109204f593ca10481dc1e555a80e9
Dominik Woźniak <degie999@gmail.com>
2020-07-13 23:06:00 +0200

    xG fetch data

---
+++ src/xG_data_scrapping.py
@@ -0,0 +1,19 @@
+# Target: Fetch xG data from understat.com
+# https://github.com/amosbastian/understat
+
+import asyncio
+import json
+import aiohttp
+from understat import Understat
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        understat = Understat(session)
+        data = await understat.get_league_players("epl", 2018, {"team_title": "Manchester United"})
+        print(json.dumps(data))
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
