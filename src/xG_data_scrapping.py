62061245ee8cb74562b398dc30481dacc4c58286
Dominik Woźniak <degie999@gmail.com>
2020-07-14 22:14:00 +0200

    xG scrapper (in progress)

--- src/xG_data_scrapping.py
+++ src/xG_data_scrapping.py
@@ -5,13 +5,59 @@ import asyncio
 import json
 import aiohttp
 from understat import Understat
+import pandas as pd
+
+TEAMS_2018 = ["Liverpool",
+              "Manchester United",
+              "Tottenham",
+              "Arsenal",
+              "Manchester United",
+              "Chelsea",
+              "Wolverhampton Wanderers",
+              "Everton",
+              "Leicester",
+              "West Ham",
+              "Watford",
+              "Crystal Palace",
+              "Bournemounth",
+              "Newcastle United",
+              "Burnley",
+              "Cardfiff",
+              "Southampton",
+              "Brighton",
+              "Fulham",
+              "Huddersfield"
+              ]
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        understat = Understat(session)
-        data = await understat.get_league_players("epl", 2018, {"team_title": "Manchester United"})
-        print(json.dumps(data))
+    for team in TEAMS_2018[:2]:
+        async with aiohttp.ClientSession() as session:
+            understat = Understat(session)
+
+            data = await understat.get_team_results(
+                team,
+                2018,
+                side="h"
+            )
+
+            # with open(f'{team}.json', 'w') as outfile:
+            #     json.dump(data, outfile, sort_keys=True, indent=4,
+            #               ensure_ascii=False)
+
+            data_to_csv = json.dumps(data)
+
+            print(json.dumps(data))
+
+        dct = {}
+
+        for match in data_to_csv:
+            dct[match['a']['title']] = match['xG']['h']
+
+        df = pd.DataFrame(dct.items(), columns=['away_team', 'xG'])
+        df['home_team'] = team
+        df = df[['home_team', 'away_team', 'xG']]
+        df.to_csv(f'{team}_2018.csv')
 
 
 if __name__ == "__main__":
