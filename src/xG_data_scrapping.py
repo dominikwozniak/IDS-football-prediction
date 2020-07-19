4f86a7ffd2b5bba34460d6cf811669bfe0bd12db
Dominik Woźniak <degie999@gmail.com>
2020-07-19 19:56:39 +0200

    xG scrapper

--- src/xG_data_scrapping.py
+++ src/xG_data_scrapping.py
@@ -12,40 +12,35 @@ from xG_teams import *
 
 
 async def main():
-    list_of_TEAMS = [TEAMS_2015, TEAMS_2016, TEAMS_2017, TEAMS_2018, TEAMS_2019]
-    years = ['2015', '2016', '2017', '2018', '2019']
-    years_fl = [2015, 2016, 2017, 2018, 2019]
-    flag_data = True
-    flag_team = True
+    
+    LIST_OF_TEAMS = [TEAMS_2015, TEAMS_2016, TEAMS_2017, TEAMS_2018, TEAMS_2019]
+    YEARS = ['2015', '2016', '2017', '2018', '2019']
+    YEARS_FLAG = [2015, 2016, 2017, 2018, 2019]
+    FLAG_DATA = True
+    FLAG_TEAM = True
+    
     index = 0
-    for teams in list_of_TEAMS:
-        if flag_data:
+    
+    for teams in LIST_OF_TEAMS:
+        if FLAG_DATA:
             os.chdir("..")
-            os.mkdir("Data")
-            os.chdir("Data")
-            flag_data = False
+            os.mkdir("data")
+            os.chdir("data")
+            FLAG_DATA = False
         for team in teams:
-            if flag_team:
-                os.mkdir("Sezon_" + years[index])
-                os.chdir("Sezon_" + years[index])
-                flag_team = False
+            if FLAG_TEAM:
+                os.mkdir("season_" + YEARS[index])
+                os.chdir("season_" + YEARS[index])
+                FLAG_TEAM = False
             async with aiohttp.ClientSession() as session:
                 understat = Understat(session)
 
                 data = await understat.get_team_results(
                     team,
-                    years_fl[index],
+                    YEARS_FLAG[index],
                     side="h"
                 )
-
-                # with open(f'{team}_raw.json', 'w') as outfile:
-                # json.dump(data, outfile, sort_keys=True, indent=4,
-                # ensure_ascii=False)
-
                 data_to_csv = data.copy()
-                # with open(f'{team}_raw.json', 'r') as read_file:
-                # data_to_csv = json.load(read_file)
-
                 print(json.dumps(data))
 
             dct = {}
@@ -56,15 +51,13 @@ async def main():
             df = pd.DataFrame(dct.items(), columns=['away_team', 'xG'])
             df['home_team'] = team
             df = df[['home_team', 'away_team', 'xG']]
-            df.to_csv(f'{team}' + '_'+ years[index] +'.csv')
+            df.to_csv(f'{team}' + '_' + YEARS[index] + '.csv')
 
         os.chdir("..")
-        flag_team = True
-        index +=1
+        FLAG_TEAM = True
+        index += 1
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-
-
